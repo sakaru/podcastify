b601f6202c3b29488e0e26a0ddb13498f77496d7
Sid Karunaratne <sid@karunaratne.net>
2019-03-13 20:17:04 +0800

    economist: revert using dynamodb transactions

--- app/economist.py
+++ app/economist.py
@@ -266,22 +266,18 @@ class Economist(BasePodcast):
             new_item[k] = {t: str(v)}
         new_item["status"] = {"S": new_status}
 
-        # Delete and put a new item
-        self.dynamodb.transact_write_items(
-            TransactItems=[{
-                "Delete": {
-                    "TableName": self.dynamodb_table,
-                    "Key": {
-                        "task_id": {"S": task_id},
-                        "status": {"S": old_status},
-                    }
-                }
-            },{
-                "Put": {
-                    "TableName": self.dynamodb_table,
-                    "Item": new_item
-                }
-            }]
+        # Delete the old item
+        self.dynamodb.delete_item(
+            TableName=self.dynamodb_table,
+            Key={
+                "task_id": {"S": task_id},
+                "status": {"S": old_status},
+            }
+        )
+        # Create the new item
+        self.dynamodb.put_item(
+            TableName=self.dynamodb_table,
+            Item=new_item
         )
 
     def _dynamodb_to_normal(self, item):
